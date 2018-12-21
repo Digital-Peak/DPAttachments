a99e45b45af7db2f3a629b742a016ec5d7f9fb44
Allon Moritz <allon.moritz@digital-peak.com>
2018-12-21 13:19:40 +0100

    Case 6596: Remove DPCases related code

--- com_dpattachments/site/views/form/tmpl/edit_actions.php
+++ com_dpattachments/site/views/form/tmpl/edit_actions.php
@@ -1,22 +1,21 @@
 <?php
 /**
- * @package    DPCases
+ * @package    DPAttachments
  * @author     Digital Peak http://www.digital-peak.com
  * @copyright  Copyright (C) 2012 - 2018 Digital Peak. All rights reserved.
  * @license    http://www.gnu.org/licenses/gpl.html GNU/GPL
  */
-
 defined('_JEXEC') or die();
 
 ?>
 <div class="com-dpattachments-form-edit__actions">
 	<div class="dp-button-group">
 		<button type="button" class="dp-button dp-button-save" data-task="save">
-			<?php echo JLayoutHelper::render('block.icon', ['icon' => \DPCases\HTML\Block\Icon::SAVE]); ?>
+			<?php echo JLayoutHelper::render('block.icon', ['icon' => 'check']); ?>
 			<?php echo JText::_('JSAVE'); ?>
 		</button>
 		<button type="button" class="dp-button dp-button-cancel" data-task="cancel">
-			<?php echo JLayoutHelper::render('block.icon', ['icon' => \DPCases\HTML\Block\Icon::CANCEL]); ?>
+			<?php echo JLayoutHelper::render('block.icon', ['icon' => 'ban']); ?>
 			<?php echo JText::_('JCANCEL'); ?>
 		</button>
 	</div>
--- com_dpattachments/site/views/form/tmpl/edit_heading.php
+++ com_dpattachments/site/views/form/tmpl/edit_heading.php
@@ -1,11 +1,10 @@
 <?php
 /**
- * @package    DPCases
+ * @package    DPAttachments
  * @author     Digital Peak http://www.digital-peak.com
  * @copyright  Copyright (C) 2012 - 2018 Digital Peak. All rights reserved.
  * @license    http://www.gnu.org/licenses/gpl.html GNU/GPL
  */
-
 defined('_JEXEC') or die();
 
 if (!$this->params->get('show_page_heading')) {
