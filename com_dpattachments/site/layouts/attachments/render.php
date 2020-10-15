4f3407289755e400e540c716cb2ffc23f055a56f
Allon Moritz <allon.moritz@digital-peak.com>
2020-10-15 14:54:14 +0200

    Add copyright to docblock

--- com_dpattachments/site/layouts/attachments/render.php
+++ com_dpattachments/site/layouts/attachments/render.php
@@ -2,8 +2,7 @@
 /**
  *
  * @package   DPAttachments
- * @author    Digital Peak http://www.digital-peak.com
- * @copyright (C) 2016 Digital Peak GmbH. <https://www.digital-peak.com>
+ * @copyright Copyright (C) 2016 Digital Peak GmbH. <https://www.digital-peak.com>
  * @license   http://www.gnu.org/licenses/gpl.html GNU/GPL
  */
 defined('_JEXEC') or die();
--- com_dpattachments/site/views/attachment/tmpl/patch_filename.php
+++ com_dpattachments/site/views/attachment/tmpl/patch_filename.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @package    DPAttachments
- * @copyright  (C) 2018 Digital Peak GmbH. <https://www.digital-peak.com>
+ * @copyright  Copyright (C) 2018 Digital Peak GmbH. <https://www.digital-peak.com>
  * @license    http://www.gnu.org/licenses/gpl.html GNU/GPL
  */
 defined('_JEXEC') or die();
